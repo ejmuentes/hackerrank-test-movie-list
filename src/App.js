693f3b83b25cfb230162782417139c95c1e8a485
Matthew Schupack <schu34@gmail.com>
2020-08-11 10:53:06 -0400

    create movielist question

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const App = () => {
-  return (<div></div>);
+const movies = [
+  { name: "Star Wars", link: "https://www.starwars.com/" },
+  { name: "The Breakfast Club", link: "https://www.imdb.com/title/tt0088847/" },
+  { name: "Alien", link: "https://www.imdb.com/title/tt0078748/" },
+];
+
+const MovieList = (props) => {
+  //complete this component to render a list of "Movie" components
+  // pass the array above as 'props.movies'
+
+  const movies = props.movies;
+  return <ul></ul>;
 };
 
+const Movie = (props) => { 
+  // Complete this component to render a single "Movie"
+  // pass a "name" and a "link" prop, render link with `href` set to 
+  // the value of the "link" prop and render the "name" inside the `a` tag. 
+  const name = props.name;
+  const link = props.link; 
+  return <li></li>;
+};
+
+const App = () => {
+  return (
+    <ul>
+      <li>
+        <a href={movies[0].link}>{movies[0].name}</a>
+      </li>
+      <li>
+        <a href={movies[1].link}>{movies[1].name}</a>
+      </li>
+      <li>
+        <a href={movies[2].link}>{movies[2].name}</a>
+      </li>
+    </ul>
+  );
+};
+
+export { MovieList, Movie, movies };
+
 export default App;
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import {
-  Grocery,
-  Fridge,
-  ShoppingCart,
-} from './App';
+import App, {Movie, MovieList, movies } from './App';
 
 /**************************************************************************************************
  * Volunteers: add you tests here. They should be failing, but the students should be able to make
@@ -14,5 +10,25 @@ import {
 
 
 describe("Tests", () => {
+  test("should render the correct html", ()=>{
+    const {getByText} = render(<App/>);
+    movies.forEach(({name})=>{
+      expect(getByText(name).textContent).toBe(name);
+    });
+  });
+
+  test("MovieList should render a list of movies", ()=>{
+    const {getByText} = render(<MovieList movies={movies}/>);
+    movies.forEach(({name})=>{
+      expect(getByText(name).textContent).toBe(name);
+    }) 
+  })
+
+  test("Movie should render a Movie", ()=>{
+    const {getByText} = render(<Movie name="testName" link="testLink" ></Movie>)
+    const content = getByText("testName");
+    expect(content.attributes.href).toBe("testLink");
+    expect(content.innerText).toBe("testName");
+  })
 });
 
